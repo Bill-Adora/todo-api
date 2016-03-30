a43768ccb004d9acd4f8917997d9267b01760695
Bill-Adora <bilstone95@gmail.com>
2016-03-30 14:23:31 +0300

    A little modification

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -17,12 +18,7 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
-    todos.forEach(function (todo) {
-        if (todoId === todo.id){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     if (matchedTodo){
         res.json(matchedTodo);
     } else {
@@ -32,7 +28,9 @@ app.get('/todos/:id', function(req, res){
 
 app.post('/todos', function(req, res) {
     var body = req.body;
-    
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(404).send();
+    }
     body.id = todoNextId++;
     todos.push(body);
     
