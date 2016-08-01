2b98bf217767d5467ce3d69655b90d9506be3f45
Bill-Adora <bilstone95@gmail.com>
2016-08-01 16:30:46 +0300

    Filter todos

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ app.use(bodyParser.json());
  });
 
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+    
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+        filteredTodos = _.where(filteredTodos, {completed : true});
+    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+        filteredTodos = _.where(filteredTodos, {completed : false});
+    }
+    res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function(req, res){
@@ -70,6 +78,7 @@ app.put('/todos/:id', function(req, res){
     _.extend(matchedTodo, validAttributes);
     res.json(matchedTodo);
 });
+
  app.listen(PORT, function () {
      console.log('Express listening on port ' + PORT +'!');
  });
